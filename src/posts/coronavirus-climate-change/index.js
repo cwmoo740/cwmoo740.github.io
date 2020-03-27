69590f8d8b45ba01e07b83fd50c687d892ba8bb8
cwmoo740 <cwmoo740@gmail.com>
2020-03-27 15:24:10 -0400

    coronavirus v climate change blog post

---
+++ src/posts/coronavirus-climate-change/index.js
@@ -0,0 +1,9 @@
+import post from './post.html';
+
+export const html = post;
+
+export const title = 'A Comparison of Climate Change and SARS-COV-2';
+
+export const slug = 'climate-change-coronavirus';
+
+export const date = new Date('2020-03-26');
--- src/posts/index.js
+++ src/posts/index.js
@@ -1,7 +1,9 @@
 import * as google from './google-irrelevance';
 import * as dark from './his-dark-materials';
+import * as coronavirus from './coronavirus-climate-change';
 
 const posts = [
+    coronavirus,
     dark,
     google,
 ]
