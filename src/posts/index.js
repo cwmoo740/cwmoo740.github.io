654ab77f9ea45980b239a2eaed6e9329d4d677ae
cwmoo740 <cwmoo740@gmail.com>
2020-05-24 22:12:57 -0400

    post: michigan lockdown protests

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,11 +1,13 @@
 import * as google from './google-irrelevance';
 import * as dark from './his-dark-materials';
 import * as coronavirus from './coronavirus-climate-change';
+import * as michigan from './michigan-lockdown-protests';
 
 const posts = [
     coronavirus,
     dark,
     google,
+    michigan,
 ]
     .sort((a, b) => a.date - b.date);
 
---
+++ src/posts/michigan-lockdown-protests/index.js
@@ -0,0 +1,9 @@
+import post from './post.html';
+
+export const html = post;
+
+export const title = 'Armed Opposition Group Shuts Down Michigan Government';
+
+export const slug = 'michigan-lockdown-protests';
+
+export const date = new Date('2020-05-16');
