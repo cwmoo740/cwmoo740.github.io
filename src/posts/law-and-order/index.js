09280e64fa9e5713833d5521c144e53682e7f2a1
cwmoo740 <cwmoo740@gmail.com>
2020-11-01 15:17:21 -0500

    rename law and order post

--- src/posts/law-and-order/index.js
+++ src/posts/law-and-order/index.js
@@ -2,7 +2,7 @@ import post from './post.html';
 
 export const html = post;
 
-export const title = `On Trump's Law And Order`;
+export const title = `On Law And Order`;
 
 export const slug = 'law-and-order';
 
